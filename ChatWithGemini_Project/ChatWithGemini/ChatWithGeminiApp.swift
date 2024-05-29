855d6532bbf4124249e82e99d8d70469243af4f1
Aryan Sharma <62754988+aryan87360@users.noreply.github.com>
2024-05-29 06:52:48 +0530

    Add files via upload

---
+++ ChatWithGemini_Project/ChatWithGemini/ChatWithGeminiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatWithGeminiApp.swift
+//  ChatWithGemini
+//
+//  Created Aryan Sharma on 03/05/24.
+//
+
+import SwiftUI
+
+@main
+struct ChatWithGeminiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
